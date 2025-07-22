e29280a6a025770dda911c252e1d2c290fc1424d
Manyim Ma Makon <nmaknyim@gmail.com>
2025-07-22 13:00:23 +0200

    module10:Fichiers Lecture et ecriture

---
+++ 10-Fichiers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace _10_Fichiers
+{
+    class Program
+    {
+        static void Main()
+        {
+            string cheminFichier = "user_text.txt";
+
+            Console.WriteLine("Tape des lignes (tape 'exit' pour quitter et sauvegarder) :");
+
+            using (StreamWriter writer = new StreamWriter(cheminFichier))
+            {
+                string ligne;
+                while (true)
+                {
+                    ligne = Console.ReadLine();
+                    if (ligne.ToLower() == "exit")
+                        break;
+
+                    writer.WriteLine(ligne);
+                }
+            }
+
+            Console.WriteLine($"\nContenu sauvegardé dans {cheminFichier} :");
+            Console.WriteLine(File.ReadAllText(cheminFichier));
+        }
+    }
+}
